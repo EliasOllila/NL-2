2b3e836ae05810751eaebabbc2dd93c9868742f3
eliasollila <elias.ollila@syk.fi>
2022-10-07 08:58:01 +0300

    joku

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("The purpose of the program is to check if there are any same numbers in the array");
+        System.out.println("You could put an additional condition");
+        boolean flag=false;
+        int[] a={0, 6, 7, 8, 9, 10};
+        int[] b={1,2,3,4,5,-2, -100, -1};
+
+
+        for(int n=0; n<a.length||flag==true; n++)
+            for(int m=0; m<b.length||flag==true; m++)
+                if(a[n]==b[m])
+                    flag=true;
+
+        System.out.println(flag);
     }
 }
